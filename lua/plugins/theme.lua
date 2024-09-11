0e3c55576987db5cb5eb823a9355110d2ed9e5b4
Soooda <kongliangxm@foxmail.com>
2024-09-11 17:11:41 +0900

    Add Colortheme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        enabled = true,
+        opts = {
+            flavour = "macchiato",
+            background = {
+                light = "latte",
+                dark = "macchiato",
+            },
+            transparent_background = true,
+            term_colors = true,
+            integrations = {
+                -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+            },
+        },
+        config = function(_, opts)
+            require("catppuccin").setup(opts)
+            vim.cmd.colorscheme("catppuccin")
+        end
+    },
+	{
+		"Mofiqul/dracula.nvim",
+		enabled = false,
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			vim.cmd.colorscheme("dracula")
+		end,
+	},
+}
