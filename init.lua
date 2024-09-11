3b386d58b7860f394fab8cd0fc54c1662729f8ca
Soooda <kongliangxm@foxmail.com>
2024-09-11 17:11:39 +0900

    Bootstrap Lazy.nvim

---
+++ init.lua
@@ -0,0 +1,30 @@
+--[[
+ __  ____   __  _   ___     _____ __  __ ____   ____
+|  \/  \ \ / / | \ | \ \   / /_ _|  \/  |  _ \ / ___|
+| |\/| |\ V /  |  \| |\ \ / / | || |\/| | |_) | |
+| |  | | | |   | |\  | \ V /  | || |  | |  _ <| |___
+|_|  |_| |_|   |_| \_|  \_/  |___|_|  |_|_| \_\\____|
+
+Author: @Soooda
+--]]
+
+-- Bootstrap Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", --A latest stable release
+		lazypath,
+	}
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+	ui = {
+		border = "rounded",
+	},
+})
+
