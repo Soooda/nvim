135bed7a432402bc5aed560097f26035ff6d2eec
Soooda <kongliangxm@foxmail.com>
2024-09-12 09:48:24 +0900

    Add Plugin Accelerated-JK

---
+++ lua/plugins/functional.lua
@@ -0,0 +1,21 @@
+return {
+	-- Improve J, K scrolling speed
+	{
+		"rainbowhxch/accelerated-jk.nvim",
+		lazy = true,
+		event = "VeryLazy",
+		opts = {
+			mode = 'time_driven',
+			enable_deceleration = false,
+			acceleration_motions = {},
+			acceleration_limit = 150,
+			acceleration_table = { 3, 6, 9, 12, 15, 18, 23, 28, 30 },
+			-- when 'enable_deceleration = true', 'deceleration_table = { {200, 3}, {300, 7}, {450, 11}, {600, 15}, {750, 21}, {900, 9999} }'
+			deceleration_table = { { 150, 9999 } }
+		},
+		config = function()
+			vim.keymap.set('n', 'j', '<Plug>(accelerated_jk_gj)', {})
+			vim.keymap.set('n', 'k', '<Plug>(accelerated_jk_gk)', {})
+		end,
+	},
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,18 +1,64 @@
 return {
+	-- Key Mappings Hint
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		opts = {
 			win = {
 				border = "rounded",
+			padding = { 0, 0, 0, 0 }, -- extra window padding [top, right, bottom, left]
 			},
 			layout = {
-				height = { min = 4, max = 15 }, -- min and max height of the columns
 				width = { min = 20, max = 50 }, -- min and max width of the columns
+				height = { min = 4, max = 15 }, -- min and max height of the columns
 				spacing = 0,
-				align = "left",
 			},
 		},
 		keys = {},
+	},
+	-- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				icons_enabled = true,
+				-- theme = 'dracula-nvim',
+				theme = 'catppuccin',
+				component_separators = '│',
+				section_separators = { left = "", right = "" },
+				ignore_focus = {},
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = {
+					{ "mode", separator = { left = "" }, right_padding = 2 },
+				},
+				-- lualine_b = { "string.format('R:%3d C:%-2d', vim.fn.line('.'), vim.fn.virtcol('.'))" },
+				lualine_b = {
+					{
+						function()
+							local loc = require("lualine.components.location")()
+							local sel = require("lualine.components.selectioncount")()
+							if sel ~= "" then
+								loc = loc .. " (" .. sel .. " sel)"
+							end
+							return loc
+						end,
+						separator = { right = "" },
+						left_padding = 2,
+					},
+				},
+				lualine_c = { 'filename' },
+				lualine_x = {},
+				lualine_y = { 'branch', 'diff', 'diagnostics' },
+				lualine_z = {
+					{ "filesize", separator = { right = "" }, left_padding = 2 },
+				},
+			},
+			extensions = {
+				"neo-tree",
+			},
+		},
 	},
 }
