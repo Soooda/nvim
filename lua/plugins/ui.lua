5d3630eac948304f07ec2e66c30dba9051658e37
Soooda <kongliangxm@foxmail.com>
2024-09-12 10:18:59 +0900

    Add Plugin lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -61,4 +61,49 @@ return {
 			},
 		},
 	},
+	-- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				icons_enabled = true,
+				-- theme = 'dracula-nvim',
+				theme = 'catppuccin',
+				component_separators = '│',
+				section_separators = { left = "", right = "" },
+				ignore_focus = {},
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = {
+					{ "mode", separator = { left = "" }, right_padding = 2 },
+				},
+				-- lualine_b = { "string.format('R:%3d C:%-2d', vim.fn.line('.'), vim.fn.virtcol('.'))" },
+				lualine_b = {
+					{
+						function()
+							local loc = require("lualine.components.location")()
+							local sel = require("lualine.components.selectioncount")()
+							if sel ~= "" then
+								loc = loc .. " (" .. sel .. " sel)"
+							end
+							return loc
+						end,
+						separator = { right = "" },
+						left_padding = 2,
+					},
+				},
+				lualine_c = { 'filename' },
+				lualine_x = {},
+				lualine_y = { 'branch', 'diff', 'diagnostics' },
+				lualine_z = {
+					{ "filesize", separator = { right = "" }, left_padding = 2 },
+				},
+			},
+			extensions = {
+				"neo-tree",
+			},
+		},
+	},
 }
