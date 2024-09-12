5c5cbdc918120dcae65f40ed47af6c570916ae51
Soooda <kongliangxm@foxmail.com>
2024-09-12 12:00:34 +0900

    Add Plugin nvim-autopairs

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,3 +1,27 @@
 return {
-	{},
+	-- Auto-pairing brackets
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {
+			disable_filetype = { "TelescopePrompt", "vim" },
+			check_ts = true,
+			enable_check_bracket_line = true,
+			fast_wrap = {
+				-- map = "<M-e>",
+				chars = { "{", "(", "[", "<", '"', "'", "`" },
+			},
+		},
+		-- TODO: remove this block when https://github.com/windwp/nvim-autopairs/pull/363 is merged
+		config = function(opts)
+			local autopairs = require("nvim-autopairs")
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+			autopairs.setup(opts)
+			autopairs.add_rules {
+				Rule("<", ">"):with_pair(cond.before_regex("%a+")):with_move(function(o) return o.char == ">" end),
+			}
+		end,
+		-- END TODO
+	},
 }
