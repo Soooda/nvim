b4e35fbe1e21dd6a4345453ed995d133c63827c4
Soooda <kongliangxm@foxmail.com>
2024-09-17 14:26:15 +0900

    Add Plugin mini.comment

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -7,7 +7,9 @@ vim.keymap.set("", "<Space>", "<Nop>")
 vim.keymap.set({ "n", "v" }, ";", ":")
 vim.keymap.set("", "<Tab>", "<Nop>") -- Will be handled in `plugins/completion.lua`
 vim.keymap.set("", "s", "<Nop>")
-vim.keymap.del('n', 'gc') -- Resolve the which-key checkhealth issue
+-- Disable built-in comment action and resolve the which-key checkhealth issue
+vim.keymap.del('n', 'gc')
+vim.keymap.del('n', 'gcc')
 
 -- Quit
 vim.keymap.set('n', 'Q', ':q<CR>', { silent = true, desc = "Quit" })
